ba41e6d728d8065f02502038c87238781866f848
K1111n <kaja_nan@hotmail.de>
2025-08-29 11:07:14 +0200

    feat: class Keyboard

--- js/game.js
+++ js/game.js
@@ -1,9 +1,46 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById("canvas");
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
     console.log('My Character is', world.character);
 }
+
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode == 87 || e.keyCode == 38) {
+        keyboard.UP = true;
+    }
+    else if (e.keyCode == 83 || e.keyCode == 40) {
+        keyboard.DOWN = true;
+    }
+    else if (e.keyCode == 65 || e.keyCode == 37) {
+        keyboard.LEFT = true;
+    }
+    else if (e.keyCode == 68 || e.keyCode == 39) {
+        keyboard.RIGHT = true;
+    }
+    else if (e.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+}) 
+
+window.addEventListener('keyup', (e) => {
+    if (e.keyCode == 87 || e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+    else if (e.keyCode == 83 || e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+    else if (e.keyCode == 65 || e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+    else if (e.keyCode == 68 || e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+    else if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+}) 
