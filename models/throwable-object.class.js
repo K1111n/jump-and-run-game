ab29c9b29202facb7d1b2b6bd217a527cf942235
K1111n <kaja_nan@hotmail.de>
2025-08-31 21:41:15 +0200

    feat: throwable objects

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,1 +1,19 @@
-class ThrowableObject extends MovableObject {}
+class ThrowableObject extends MovableObject {
+
+    constructor(x, y) {
+        super().loadImage('../img/6_salsa_bottle/salsa_bottle.png');
+        this.x = x;
+        this.y = y;
+        this.width = 50;
+        this.height = 60;
+        this.throw();
+    }
+
+    throw() {
+        this.speedY = 30;
+        this.applyGravity();
+        setInterval(() => {
+            this.x += 10;
+        }, 1000 / 60);
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -7,6 +7,7 @@ class World {
     keyboard;
     camera_x = 0;
     statusBar = new StatusBar();
+    throwableObjects = [];
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -14,22 +15,34 @@ class World {
         this.keyboard = keyboard
         this.draw();
         this.setWorld();
-        this.checkCollisions();
+        this.run();
     }
 
     setWorld() {
         this.character.world = this;
     }
   
-    checkCollisions() {
+    run() {
         setInterval(() => {
-            this.level.enemies.forEach((enemy) => {
+            this.checkCollisions();
+            this.checkThrowObbjects();
+        }, 200)
+    }
+
+    checkThrowObbjects() {
+        if(this.keyboard.D) {
+            let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+            this.throwableObjects.push(bottle);
+        }
+
+    }
+    checkCollisions() {
+        this.level.enemies.forEach((enemy) => {
                 if(this.character.isColliding(enemy) ) {
                     this.character.hit();
                     this.statusBar.setPercentage(this.character.energy);
                 }
             })
-        }, 1000)
     }
 
     draw() {
@@ -46,6 +59,7 @@ class World {
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.throwableObjects);
         
         this.ctx.translate(-this.camera_x, 0);
 
