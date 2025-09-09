07ea3ec5c3e2ee9908de5d141dbaed8b3ebfeb87
Kajanan Yoganathan <kaja_nan@hotmail.de>
2025-09-09 17:15:20 +0200

    fix: startScreen Dialog, endScreen Dialog

--- js/game.js
+++ js/game.js
@@ -4,57 +4,52 @@ let world;
 let keyboard = new Keyboard();
 let gameStarted = false;
 let gameEnded = false;
-let startScreen;
-let endScreen;
+let statusInterval;
+
+let startDialog;
+let winDialog;
+let loseDialog;
 
 function init() {
     canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d'); 
-    // world = new World(canvas, keyboard);
-    showStartScreen();
-    setupStartListener();
-}
-
-function showStartScreen() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx = canvas.getContext('2d');
     
-    let startImg = new Image();
-    startImg.src = '../img/9_intro_outro_screens/start/startscreen_2.png';
-    startImg.onload = () => {
-        ctx.drawImage(startImg, 0, 0, canvas.width, canvas.height);
-        
-        ctx.fillStyle = 'white';
-        ctx.font = '30px Arial';
-        ctx.textAlign = 'center';
-        ctx.fillText('Press ENTER to start', canvas.width/2, canvas.height - 50);
-    };
+    startDialog = document.getElementById('startDialog');
+    winDialog = document.getElementById('winDialog');
+    loseDialog = document.getElementById('loseDialog');
+    
+    startDialog.showModal();
+    
+    window.addEventListener('keydown', handleEnterStart);
 }
 
-function setupStartListener() {
-    function startGame(event) {
-        if (!gameStarted && event.key === "Enter") {
-            gameStarted = true;
-            window.removeEventListener('keydown', startGame);
-            initGame();
-        }
+function handleEnterStart(event) {
+    if (event.key === "Enter" && !gameStarted) {
+        startGame();
     }
-    window.addEventListener('keydown', startGame);
+}
+
+function startGame() {
+    window.removeEventListener('keydown', handleEnterStart);
+    startDialog.close();
+    gameStarted = true;
+    gameEnded = false;
+    initGame();
 }
 
 function initGame() {
     world = new World(canvas, keyboard);
-    
     checkGameStatus();
 }
 
 function checkGameStatus() {
-    let statusInterval = setInterval(() => {
+    statusInterval = setInterval(() => {
         if (!gameEnded && world && world.character) {
             if (world.character.isDead() && world.character.deathAnimationComplete) {
                 gameEnded = true;
                 clearInterval(statusInterval);
                 setTimeout(() => {
-                    showEndScreen(false); // false = lost
+                    showEndDialog(false);
                 }, 1000);
             }
             
@@ -63,92 +58,94 @@ function checkGameStatus() {
                 gameEnded = true;
                 clearInterval(statusInterval);
                 setTimeout(() => {
-                    showEndScreen(true); // true = won
+                    showEndDialog(true);
                 }, 1000);
             }
         }
     }, 100);
 }
 
-function showEndScreen(won) {
+function showEndDialog(won) {
     if (world) {
+        if (won) {
+            winDialog.showModal();
+        } else {
+            loseDialog.showModal();
+        }
         world = null;
     }
+}
+
+function restartGame() {
+    winDialog.close();
+    loseDialog.close();
+    
+    gameStarted = false;
+    gameEnded = false;
+    keyboard = new Keyboard();
     
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    let endImg = new Image();
-    if (won) {
-        endImg.src = '../img/You won, you lost/You won A.png';
-    } else {
-        endImg.src = '../img/You won, you lost/You lost b.png';
-    }
+    startDialog.showModal();
     
-    endImg.onload = () => {
-        ctx.drawImage(endImg, 0, 0, canvas.width, canvas.height);
-        
-        ctx.fillStyle = 'white';
-        ctx.font = '25px Arial';
-        ctx.textAlign = 'center';
-        ctx.fillText('Press F5 to restart', canvas.width/2, canvas.height - 30);
-    };
+    window.addEventListener('keydown', handleEnterStart);
 }
 
 window.addEventListener('keydown', (event) => {
     if (gameStarted && !gameEnded) {
-    if (event.key === "ArrowUp") {
-        keyboard.UP = true;
+        if (event.key === "ArrowUp") {
+            keyboard.UP = true;
+        }
+        else if (event.key === "ArrowDown") {
+            keyboard.DOWN = true;
+        }
+        else if (event.key === "ArrowLeft") {
+            keyboard.LEFT = true;
+        }
+        else if (event.key === "ArrowRight") {
+            keyboard.RIGHT = true;
+        }
+        else if (event.key === " ") {
+            keyboard.SPACE = true;
+        }
+        else if (event.key === "d" || event.key === "D") {
+            keyboard.D = true;
+        }
     }
-    else if (event.key === "ArrowDown") {
-        keyboard.DOWN = true;
-    }
-    else if (event.key === "ArrowLeft") {
-        keyboard.LEFT = true;
-    }
-    else if (event.key === "ArrowRight") {
-        keyboard.RIGHT = true;
-    }
-    else if (event.key === " ") {
-        keyboard.SPACE = true;
-    }
-    else if (event.key === "D") {
-        keyboard.D = true;
-    }
-}
-}) 
+});
 
 window.addEventListener('keyup', (event) => {
     if (gameStarted && !gameEnded) {
-    if (event.key === "ArrowUp") {
-        keyboard.UP = false;
+        if (event.key === "ArrowUp") {
+            keyboard.UP = false;
+        }
+        else if (event.key === "ArrowDown") {
+            keyboard.DOWN = false;
+        }
+        else if (event.key === "ArrowLeft") {
+            keyboard.LEFT = false;
+        }
+        else if (event.key === "ArrowRight") {
+            keyboard.RIGHT = false;
+        }
+        else if (event.key === " ") {
+            keyboard.SPACE = false;
+        }
+        else if (event.key === "d" || event.key === "D") {
+            keyboard.D = false;
+        }
     }
-    else if (event.key === "ArrowDown") {
-        keyboard.DOWN = false;
-    }
-    else if (event.key === "ArrowLeft") {
-        keyboard.LEFT = false;
-    }
-    else if (event.key === "ArrowRight") {
-        keyboard.RIGHT = false;
-    }
-    else if (event.key === " ") {
-        keyboard.SPACE = false;
-    }
-    else if (event.key === "D") {
-        keyboard.D = false;
-    }
-}
-}) 
-
+});
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "F11") {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
-      return;
+    if (event.key === "F11") {
+        event.preventDefault();
+        if (document.fullscreenElement) {
+            document.exitFullscreen();
+        } else {
+            canvas.requestFullscreen().catch((err) => {
+                console.error(`Error enabling fullscreen: ${err.message}`);
+            });
+        }
     }
-    canvas.requestFullscreen().catch((err) => {
-      console.error(`Error enabling fullscreen: ${err.message}`);
-    });
-  }
 });
