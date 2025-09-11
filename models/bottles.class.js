fd0cb2c9239e344163703b2aefbfa4b15a49c3fd
Kajanan Yoganathan <kaja_nan@hotmail.de>
2025-09-11 08:17:33 +0200

    fix: fixed paths

--- models/bottles.class.js
+++ models/bottles.class.js
@@ -1,12 +1,12 @@
 class Bottles extends DrawableObject {  
 
     IMAGES_BOTTLE_ON_GROUND = [
-            '../img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
-            '../img/6_salsa_bottle/2_salsa_bottle_on_ground.png',
+            '/img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
+            '/img/6_salsa_bottle/2_salsa_bottle_on_ground.png',
     ]
 
     constructor() {
-        super().loadImage('../img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
+        super().loadImage('/img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
         this.loadImages(this.IMAGES_BOTTLE_ON_GROUND);
         this.width = 70;
         this.height = 70;
--- models/coin.class.js
+++ models/coin.class.js
@@ -1,12 +1,12 @@
 class Coin extends DrawableObject {
 
     IMAGES = [
-        '../img/8_coin/coin_1.png',
-        '../img/8_coin/coin_2.png',
+        '/img/8_coin/coin_1.png',
+        '/img/8_coin/coin_2.png',
     ];
 
     constructor() {
-        super().loadImage('../img/8_coin/coin_1.png');
+        super().loadImage('/img/8_coin/coin_1.png');
         this.loadImages(this.IMAGES);
         this.width = 120;
         this.height = 120;
